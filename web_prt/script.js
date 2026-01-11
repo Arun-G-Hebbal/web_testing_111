4714147a0da329e12754015e5adca32b633802de
Arun-G-Hebbal <arunghebbal3@gmail.com>
2026-01-11 13:21:52 +0530

    Initial files added
    Review it

---
+++ web_prt/script.js
@@ -0,0 +1,9 @@
+function changeMessage() {
+    document.getElementById("message").innerText =
+        "You just made your first Git commit! 🎉";
+}
+
+function resetMessage() {
+    document.getElementById("message").innerText =
+        "Click the button to change this text.";
+}
